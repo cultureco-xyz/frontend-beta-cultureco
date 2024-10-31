3fcac5ff7992a80d963dc3e11cada54a47b9caf0
“John <johnswaroop28@gmail.com>
2024-10-31 17:55:57 +0530

    config update

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env: {
-        GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID
+        GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID || "817174432063-mtvlsvcn3aiv448m49se34sj1lt73n8d.apps.googleusercontent.com"
     },
     async rewrites() {
         return [
             {
                 source: '/backend/:path*',
-                destination: `${process.env.BACKEND_URL}/:path*`, // Replace with your backend URL
+                destination: `${process.env.BACKEND_URL || "https://api.cultureco.xyz"}/:path*`, // Replace with your backend URL
             },
         ];
     },
