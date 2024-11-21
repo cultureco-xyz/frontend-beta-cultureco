dd3be027dc7dfdfc2529ba083df7ec015e0ae3ea
Vaibhav <vaibhav@cultureco.xyz>
2024-11-21 12:15:42 +0530

    completing explore page
    products carousel also ready, detailed view implemented, more polishing required

---
+++ src/assets/svgs/loading-cc.tsx
@@ -0,0 +1,11 @@
+import BarLoader from "./bar-loaders.icon";
+
+const CultureCoLoadingIcon = () => {
+  return (
+    <div className="grid place-content-center px-4 py-24">
+      <BarLoader />
+    </div>
+  );
+};
+
+export default CultureCoLoadingIcon;
